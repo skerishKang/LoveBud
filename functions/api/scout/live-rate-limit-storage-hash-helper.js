36d8567b87278102d3d8309e644ca090b9097f24
skerishKang <198730@jnu.ac.kr>
2026-06-09 18:03:27 +0900

    fix(scout): complete disabled storage hash helper

--- functions/api/scout/live-rate-limit-storage-hash-helper.js
+++ functions/api/scout/live-rate-limit-storage-hash-helper.js
@@ -1,5 +1,4 @@
 export const VERSION='20260609-1';
 export const DISABLED='STORAGE_HASH_HELPER_DISABLED';
 export const ALLOWED_INPUTS=Object.freeze(['userKeyHash','ipHash','sessionKeyHash','endpointPath','providerMode','limitName','windowKey']);
-export const PROHIBITED_INPUTS=Object.freeze(['token','authorization','rawUserId','email','phone','apiKey','prompt','excerpt','sourceUrl','rawRequestBody','rawProviderResponse','rawModelOutput']);
-export function createScoutLiveRate
+export const PROHIBITED_INPUTS=Object.freeze(['token','authorization','rawUserId','email
